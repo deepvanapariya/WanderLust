2a9d4a6a43f158ba73f90f540b4a74eb6c46c75a
Deep <deep.71.v@gmail.com>
2024-01-11 11:00:32 +0530

    success&failure flash msg added

--- routes/listing.js
+++ routes/listing.js
@@ -30,14 +30,20 @@ router.get("/new", (req, res) => {
 router.get("/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id).populate("reviews");
+    if (!listing) {
+        req.flash("error", "Listing you requested for does not exist");
+        res.redirect("/listings");
+    }
     res.render("listings/show.ejs", { listing });
 }))
 
 /* Create Route*/
 router.post("/", validateListing, wrapAsync(async (req, res) => {
 
+
     const newListing = new Listing(req.body.listing);
     newListing.save();
+    req.flash("success", "New Listing Created!");
     res.redirect("/listings");
 
 })
@@ -48,6 +54,10 @@ router.get("/:id/edit", wrapAsync(async (req, res) => {
 
     let { id } = req.params;
     const listing = await Listing.findById(id);
+    if (!listing) {
+        req.flash("error", "Listing you requested for does not exist");
+        res.redirect("/listings");
+    }
     res.render("listings/edit.ejs", { listing });
 
 }))
@@ -57,6 +67,7 @@ router.put("/:id", validateListing, wrapAsync(async (req, res) => {
 
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    req.flash("success", "Listing Updated!");
     res.redirect(`/listings/${id}`);
 
 })
@@ -67,6 +78,7 @@ router.delete("/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
+    req.flash("success", "Listing Deleted!");
     res.redirect("/listings");
 
 }))
