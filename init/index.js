c3366b14c8ef455908db16bb650cc5a970a36c33
Deep <deep.71.v@gmail.com>
2024-01-12 18:44:00 +0530

    redirect error

--- init/index.js
+++ init/index.js
@@ -17,6 +17,7 @@ async function main() {
 
 const initDB = async () => {
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({ ...obj, owner: "659fe6bafbe5dee97522c012" }));
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
 }
--- middleware.js
+++ middleware.js
@@ -1,8 +1,16 @@
 module.exports.isLoggedIn = (req, res, next) => {
-
     if (!req.isAuthenticated()) {
+        //redirectUrl save
+        req.session.redirectUrl = req.originalUrl;
         req.flash("error", "you must be loged in to create listings!");
         return res.redirect("/login");
+    }
+    next();
+}
+
+module.exports.saveRedirectUrl = (req, res, next) => {
+    if (req.session, redirectUrl) {
+        res.locals.redirectUrl = req.session.redirectUrl;
     }
     next();
 }
